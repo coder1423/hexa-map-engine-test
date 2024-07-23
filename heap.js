f07bdd886d14cdd33eef1c29df5fd5a5977f35dc
coder1423 <add000775@gmail.com>
2024-07-23 13:07:48 +0900

    길찾기

---
+++ heap.js
@@ -0,0 +1,27 @@
+//@ts- check
+
+export class Heap {
+  /**
+   * @param {(a: Number, b: Number) => Number} compareFn
+   * @param {Number[]} values
+   */
+  constructor(compareFn = (a,b)=>b-a, values = []) {
+    /** @type {Number[]} */
+    values.sort(compareFn);
+
+    /** @param {Number} value */
+    this.push = value => {
+      values.push(value);
+      values.sort(compareFn);
+    }
+    this.pop = values.pop;
+
+    this[Symbol.iterator] = function*() {
+      for (
+        let elem = values.pop();
+        elem !== undefined;
+        elem = values.pop()
+      ) yield elem;
+    }
+  }
+}
