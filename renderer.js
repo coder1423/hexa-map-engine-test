346bb2b9da4511d4c5dbcc8c30df45072d2b8176
coder1423 <add000775@gmail.com>
2024-07-26 17:18:38 +0900

    타일렌더링최적화(stroke 제거)

--- renderer.js
+++ renderer.js
@@ -30,54 +30,61 @@ export class Renderer{
  */
 function draw(ctx, 화면위치, 격자크기) {
   ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
-  const 타일크기 = [격자크기[x]*2, 격자크기[y]*3];
 
-  const 시작 = Vector2.divfloor(Vector2.scalarMul(화면위치, -1), 타일크기);
-  const 끝 = Vector2.add(시작, Vector2.divfloor([ctx.canvas.width, ctx.canvas.height], 타일크기)).map(val => Math.min(val+1, 100));
-
-  ctx.strokeStyle = 'rgb(0, 0, 0)';
-  ctx.lineWidth = 격자크기[y] / 10; // 격자크기[y] == 격자배율 / 2
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-
-
-  for (let 위치X = Math.max(시작[x]-1, 0); 위치X <= 끝[x]; 위치X++)
-  for (let 위치Y = Math.max(시작[y]-1, 0); 위치Y <= 끝[y]; 위치Y++) {
-    const 그리기위치 = locationToDrawingVector([위치X, 위치Y], 화면위치, 격자크기);
-      ctx.save();
-      ctx.translate(그리기위치[x], 그리기위치[y]);
-      if (위치X == 0 && 위치Y == 0) {
-        ctx.fillStyle = 'rgb(255, 0, 0)';
-      }
-      drawTilePath(ctx, 격자크기);
-      ctx.fill();
-      ctx.stroke();
-    ctx.restore();
+  // 격자크기[y] == 격자배율 / 2
+  const padding = 격자크기[y] / 20, padding격자크기 = Vector2.add(격자크기, [-padding,-padding]);
+  for (const 위치 of 출력위치(화면위치, 격자크기, [ctx.canvas.width, ctx.canvas.height])) {
+    const drawingVector = locationToDrawingVector(위치, 화면위치, 격자크기, padding);
+    if (위치[x] == 0 && 위치[y] == 0) {
+      ctx.fillStyle = 'rgb(255, 0, 0)';
+    } else {
+      ctx.fillStyle = 'rgb(255, 255, 255)';
+    }
+    drawTilePath(ctx, drawingVector, padding격자크기);
+    ctx.fill();
   }
 }
 
 /**
  * @param {CanvasRenderingContext2D} ctx
+ * @param {Number[]} drawingVector
  * @param {Number[]} 격자크기
  */
-function drawTilePath(ctx, [dx, dy]) {
+function drawTilePath(ctx, [px, py], [dx, dy]) {
   ctx.beginPath();
-  ctx.moveTo(0   ,dy  );
-  ctx.lineTo(dx  ,0   );
-  ctx.lineTo(dx*2,dy  );
-  ctx.lineTo(dx*2,dy*3);
-  ctx.lineTo(dx  ,dy*4);
-  ctx.lineTo(0   ,dy*3);
+  ctx.moveTo(px     ,py+dy  );
+  ctx.lineTo(px+dx  ,py     );
+  ctx.lineTo(px+dx*2,py+dy  );
+  ctx.lineTo(px+dx*2,py+dy*3);
+  ctx.lineTo(px+dx  ,py+dy*4);
+  ctx.lineTo(px     ,py+dy*3);
   ctx.closePath();
+}
+
+/**
+ * @param {Number[]} 화면위치
+ * @param {Number[]} 격자크기
+ * @param {Number[]} 화면크기
+ */
+function* 출력위치(화면위치, 격자크기, 화면크기) {
+  const 타일크기 = [격자크기[x]*2, 격자크기[y]*3];
+  const 시작 = Vector2.divfloor(Vector2.scalarMul(화면위치, -1), 타일크기);
+  const 끝 = Vector2.add(시작, Vector2.divfloor(화면크기, 타일크기)).map(val => Math.min(val+1, 100));
+  for (let 위치Y = Math.max(시작[y]-1, 0); 위치Y <= 끝[y]; 위치Y++)
+  for (let 위치X = Math.max(시작[x]-1, 0); 위치X <= 끝[x]; 위치X++) {
+    yield [위치X, 위치Y];
+  }
 }
 
 /**
  * @param {Number[]} location
  * @param {Number[]} 화면위치
  * @param {Number[]} 격자크기
-*/
-function locationToDrawingVector(location, 화면위치, 격자크기) {
+ * @param {Number} padding
+ */
+function locationToDrawingVector(location, 화면위치, 격자크기, padding) {
   return [
-    화면위치[x] + location[x]*격자크기[x]*2 + (location[y] & 1)*격자크기[x],
-    화면위치[y] + location[y]*격자크기[y]*3
+    화면위치[x] + padding + location[x]*격자크기[x]*2 + (location[y] & 1)*격자크기[x],
+    화면위치[y] + padding + location[y]*격자크기[y]*3
   ]
 }
