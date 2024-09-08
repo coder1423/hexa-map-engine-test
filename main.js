03fe6a16328b26bc54177c1b25f1fb8d78141bb5
coder1423 <add000775@gmail.com>
2024-09-08 15:41:38 +0900

    이름규칙

--- main.js
+++ main.js
@@ -2,6 +2,31 @@
 import {Client} from './client/client.js';
 import * as Map from './map-data.js';
 
+/** 
+ * objectName
+ * ClassName
+ * funcName
+ * file-name
+ * folder-name
+ */
+
+/**
+ * show    : 보여주기
+ * 
+ * get     : 값 반환
+ * calc    : 계산 값 반환
+ * create  : 객체 생성 및 반환?
+ * check   : 상태 논리값 반환
+ * is      : 타입 논리값 반환
+ * transform??
+ * 
+ * handle  : 이벤트 처리
+ * set     : 값 설정
+ * update  : 상태 변경
+ * clear   : 지우기
+ * perform : 작업 수행
+ */
+
 /**
  * 브라우져 전역 데이터
  */
