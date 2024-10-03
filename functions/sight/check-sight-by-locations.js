53eaaa82ef2c65c647baaa03e43ba75b194b76e5
coder1423 <add000775@gmail.com>
2024-10-03 19:36:37 +0900

    시야 함수 추가 및 기존 함수 이름 변경

--- functions/sight/check-sight-by-locations.js
+++ functions/sight/check-sight-by-locations.js
@@ -1,5 +1,198 @@
 // @ts-check
+import * as Location from '../location.js';
 
-export function checkSightByLocations() {
+/**
+ * 참이면 보이는것, 거짓이면 가려진것
+ * @param {Number[]} reference
+ * @param {Number[]} target
+ * @param {Number[]} max
+ * @param {Number[]} 지형높이
+ * @param {Number[]} 지형지도
+ */
+export function checkSightByLocations(reference, target, max, 지형높이, 지형지도) {
+  const referenceIndex = Location.getIndexByLocation(reference, max);
+  if (referenceIndex === undefined) return;
 
+  const relativeLocation = Location.getRelativeLocationByLocations(reference, target),
+    referenceDistance = Location.getDistanceByRelativeLocation(relativeLocation),
+    reference높이 = 지형높이[지형지도[referenceIndex]],
+    /** @type {(location: Number[], distance: Number) => Number | undefined} */
+    getSlopeByLocation = (location, distance) => {
+      const index = Location.getIndexByLocation(location, max);
+      if (index === undefined) return;
+      return (지형높이[지형지도[index]] - reference높이) / distance;
+    },
+    referenceSlope = getSlopeByLocation(target, referenceDistance);
+  if (referenceSlope === undefined) return;
+
+  /** @type {(location: Number[], distance: Number) => Boolean} 참이면 가려진것 */
+  const isObstructingByLocation = (location, distance) => {
+    const slope = getSlopeByLocation(location, distance);
+    return slope === undefined? true : referenceSlope < slope;
+  }
+
+  return checkSight(reference, relativeLocation, isObstructingByLocation);
 }
+
+/**
+ * @param {Number[]} reference
+ * @param {Number[]} relativeLocation
+ * @param {(location: Number[], distance: Number) => Boolean} isObstructingByLocation
+ */
+function checkSight(reference, [x, y], isObstructingByLocation) {
+  return x == 0? 대각(
+    reference, isObstructingByLocation, 대각Direction[y < 0? 4 : 5],
+    Math.abs(y) >>> 1
+  ) : y == 0? 직선(
+    reference, isObstructingByLocation, 직선Direction[x < 0? 4 : 5],
+    Math.abs(x)
+  ) : 사분면(
+    reference, isObstructingByLocation,
+    get규격LocationByLocation([x, y]),
+    getKeyByLocation([x, y])
+  );
+}
+/**
+ * @param {Number[]} reference
+ * @param {(location: Number[], distance: Number) => Boolean} isObstructingByLocation
+ * @param {Number[]} 규격Location
+ * @param {Number} key
+ */
+function 사분면(reference, isObstructingByLocation, [x, y], key) {
+  return x < y? 측면(
+    reference, isObstructingByLocation, high측면Direction[key],
+    Math.ceil((y - x) * 0.5),
+    x
+  ) : x == y? 직선(
+    reference, isObstructingByLocation, 직선Direction[key],
+    y
+  ) : x < y*3? 측면(
+    reference, isObstructingByLocation, medium측면Direction[key],
+    Math.ceil((x - y) * 0.5),
+    Math.ceil((y*3 - x) * 0.5)
+  ) : x == y*3? 대각(
+    reference, isObstructingByLocation, 대각Direction[key],
+    y
+  ) : 측면(
+    reference, isObstructingByLocation, low측면Direction[key],
+    y,
+    Math.ceil((x - y*3) * 0.5)
+  );
+}
+
+/**
+ * @param {Number[]} location
+ * @param {(location: Number[], distance: Number) => Boolean} isObstructingByLocation
+ * @param {Number[]} direction
+ * @param {Number} count
+ */
+function 직선(location, isObstructingByLocation, direction, count) {
+
+  for (let distance = 1; distance <= count; distance++) {
+    location = Location.getLocationByRelativeLocation(location, direction);
+    if (isObstructingByLocation(location, distance)) return false;
+  }
+
+  return true;
+}
+
+/**
+ * @param {Number[]} location
+ * @param {(location: Number[], distance: Number) => Boolean} isObstructingByLocation
+ * @param {Number[][]} directions
+ * @param {Number} count
+ */
+function 대각(location, isObstructingByLocation, [direction, 좌측면, 우측면], count) {
+  let 중심Location = location;
+
+  for (let distance = 1; distance <= count; distance++) {
+    중심Location = Location.getLocationByRelativeLocation(중심Location, direction);
+    if (isObstructingByLocation(location, distance)) return false;
+  }
+
+  return [좌측면, 우측면].some(
+    측면 => 대각직선(Location.getLocationByRelativeLocation(location, 측면), isObstructingByLocation, direction, count)
+  );
+}
+
+/**
+ * @param {Number[]} location
+ * @param {(location: Number[], distance: Number) => Boolean} isObstructingByLocation
+ * @param {Number[]} direction
+ * @param {Number} count
+ */
+function 대각직선(location, isObstructingByLocation, direction, count) {
+
+  for (let distance = 1; distance <= count; distance++) {
+    if (isObstructingByLocation(location, (distance << 1)+1)) return false;
+    location = Location.getLocationByRelativeLocation(location, direction);
+  }
+
+  return true;
+}
+
+/**
+ * @param {Number[]} location
+ * @param {(location: Number[], distance: Number) => Boolean} isObstructingByLocation
+ * @param {Number[][]} directions
+ * @param {Number} 직선횟수
+ * @param {Number} 측면횟수
+ */
+function 측면(location, isObstructingByLocation, [직선Direction, 측면Direction], 직선횟수, 측면횟수) {
+
+  for (let 직선distance = 0; 직선distance <= 직선횟수; 직선횟수++) {
+    let 측면Location = location;
+
+    for (let 측면Distance = 0; 측면Distance <= 측면횟수; 측면Distance++) {
+      const distance = (직선distance << 1) + 측면Distance;
+      if (distance == 0) continue;
+      if (isObstructingByLocation(측면Location, distance)) return false;
+      
+      측면Location = Location.getLocationByRelativeLocation(측면Location, 측면Direction);
+    }
+
+    location =  Location.getLocationByRelativeLocation(location, 직선Direction);
+  }
+
+  return true;
+}
+
+/** @param {Number[]} location */
+function get규격LocationByLocation([x, y]) {
+  return [
+    (Math.abs(x) << 1) - (y & 1),
+    Math.abs(y)
+  ]
+}
+/** @param {Number[]} location */
+function getKeyByLocation([x, y]) {
+  return (x < 0? 0 : 1) + (y < 0? 0 : 2);
+}
+
+const 직선Direction = [
+  [-1,-1], [ 1,-1],
+  [-1, 1], [ 1, 1],
+  [-1, 0], [ 1, 0]
+], 대각Direction = [
+  [[-2,-1], [-1, 0], [-1,-1]],
+  [[ 2,-1], [ 1,-1], [ 1, 0]],
+  [[-2, 1], [-1, 1], [-1, 0]],
+  [[ 2, 1], [ 1, 0], [ 1, 1]],
+  [[ 0,-2], [-1,-1], [ 1,-1]],
+  [[ 0, 2], [ 1, 1], [-1, 1]]
+], high측면Direction = [
+  [[0, 2], [-1,-1]],
+  [[0, 2], [ 1,-1]],
+  [[0,-2], [-1, 1]],
+  [[0,-2], [ 1, 1]]
+], medium측면Direction = [
+  [[-2,-1], [-1,-1]],
+  [[ 2,-1], [ 1,-1]],
+  [[-2, 1], [-1, 1]],
+  [[ 2, 1], [ 1, 1]]
+], low측면Direction = [
+  [[-2,-1], [-1, 0]],
+  [[ 2,-1], [ 1, 0]],
+  [[-2, 1], [-1, 0]],
+  [[ 2, 1], [ 1, 0]]
+]
--- functions/sight/get-locations-by-sight.js
+++ functions/sight/get-locations-by-sight.js
@@ -2,85 +2,85 @@
 import * as Location from '../location.js';
 
 /**
- * @param {Number} 최대거리
- * @param {Number[]} 시작
+ * @param {Number} maxDistance
+ * @param {Number[]} start
  * @param {Number[]} max
- * @param {(index: Number) => Number} get높이by인덱스
+ * @param {(index: Number) => Number} get높이ByIndex
  */
-export function getLocationsBySight(최대거리, 시작, max, get높이by인덱스) {
-  const 시작인덱스 = Location.getIndexByLocation(시작, max),
-    보임집합 = new Set([시작인덱스]),
-    가려짐지도 = [];
-  if (시작인덱스 === undefined) return 보임집합;
-  const 시작높이 = get높이by인덱스(시작인덱스);
-  가려짐지도[시작인덱스] = 시작높이;
+export function getLocationsBySight(maxDistance, start, max, get높이ByIndex) {
+  const startIndex = Location.getIndexByLocation(start, max),
+    visibleSet = new Set([startIndex]),
+    obscured지도 = [];
+  if (startIndex === undefined) return visibleSet;
+  const start높이 = get높이ByIndex(startIndex);
+  obscured지도[startIndex] = start높이;
 
-  for (const 방향 of 직선방향들)
-  for (let 기존 = 시작, 기존인덱스 = 시작인덱스, i = 최대거리; i--;) {
-    const 대상 = Location.getLocationByRelativeLocation(기존, 방향),
-      대상인덱스 = Location.getIndexByLocation(대상, max);
+  for (const direction of 직선Directions)
+  for (let previous = start, previousIndex = startIndex, count = maxDistance; count--;) {
+    const target = Location.getLocationByRelativeLocation(previous, direction),
+      targetIndex = Location.getIndexByLocation(target, max);
 
-    if (대상인덱스 === undefined) break;
-    const 가려짐 = 가려짐지도[기존인덱스],
-      높이 = get높이by인덱스(대상인덱스);
-    if (가려짐 <= 시작높이) 보임집합.add(대상인덱스);
-    가려짐지도[대상인덱스] = Math.max(높이, 가려짐);
+    if (targetIndex === undefined) break;
+    const obscured = obscured지도[previousIndex],
+      높이 = get높이ByIndex(targetIndex);
+    if (obscured <= start높이) visibleSet.add(targetIndex);
+    obscured지도[targetIndex] = Math.max(높이, obscured);
 
-    기존 = 대상;
-    기존인덱스 = 대상인덱스;
+    previous = target;
+    previousIndex = targetIndex;
   }
 
-  for (const {기준방향, 직선방향들} of 기준직선방향들)
-  for (let 이전기준 = 시작, i = 최대거리; 0 <= (i -= 2);) {
-    const 지금기준 = Location.getLocationByRelativeLocation(이전기준, 기준방향),
-      기준인덱스 = Location.getIndexByLocation(지금기준, max);
+  for (const {referenceDirection, 직선Directions} of reference직선Directions)
+  for (let previousReference = start, count = maxDistance; 0 <= (count -= 2);) {
+    const currentReference = Location.getLocationByRelativeLocation(previousReference, referenceDirection),
+      referenceIndex = Location.getIndexByLocation(currentReference, max);
 
     /** @type {Number | undefined} */
-    let 가려짐;
-    for (const 방향 of 직선방향들) {
-      const index = Location.getIndexByLocation(Location.getLocationByRelativeLocation(이전기준, 방향), max);
-      if (index === undefined || 가려짐 !== undefined && 가려짐 < 가려짐지도[index]) continue;
-      가려짐 = 가려짐지도[index];
+    let obscured;
+    for (const direction of 직선Directions) {
+      const index = Location.getIndexByLocation(Location.getLocationByRelativeLocation(previousReference, direction), max);
+      if (index === undefined || obscured !== undefined && obscured < obscured지도[index]) continue;
+      obscured = obscured지도[index];
     }
 
-    if (가려짐 === undefined || 기준인덱스 === undefined) break;
-    const 높이 = get높이by인덱스(기준인덱스);
-    if (가려짐 <= 시작높이) 보임집합.add(기준인덱스);
-    가려짐지도[기준인덱스] = Math.max(높이, 가려짐);
+    if (obscured === undefined || referenceIndex === undefined) break;
+    const 높이 = get높이ByIndex(referenceIndex);
+    if (obscured <= start높이) visibleSet.add(referenceIndex);
+    obscured지도[referenceIndex] = Math.max(높이, obscured);
 
-    for (const 방향 of 직선방향들)
-    for (let 이전비교 = 이전기준, 기존인덱스 = 기준인덱스, j = i; j--;) {
-      const 비교 = Location.getLocationByRelativeLocation(이전비교, 방향),
-        대상 = Location.getLocationByRelativeLocation(비교, 기준방향),
-        비교인덱스 = Location.getIndexByLocation(비교, max),
-        대상인덱스 = Location.getIndexByLocation(대상, max);
+    for (const direction of 직선Directions)
+    for (let previousComparison = previousReference, previousIndex = referenceIndex, count2 = count; count2--;) {
+      const comparison = Location.getLocationByRelativeLocation(previousComparison, direction),
+        target = Location.getLocationByRelativeLocation(comparison, referenceDirection),
+        comparisonIndex = Location.getIndexByLocation(comparison, max),
+        targetIndex = Location.getIndexByLocation(target, max);
 
-      if (대상인덱스 === undefined || 비교인덱스 === undefined) break;
-      const 가려짐 = Math.max(가려짐지도[기존인덱스], 가려짐지도[비교인덱스]),
-        높이 = get높이by인덱스(대상인덱스);
-      if (가려짐 <= 시작높이) 보임집합.add(대상인덱스);
-      가려짐지도[대상인덱스] = Math.max(높이, 가려짐);
+      if (targetIndex === undefined || comparisonIndex === undefined) break;
+      const obscured = Math.max(obscured지도[previousIndex], obscured지도[comparisonIndex]),
+        높이 = get높이ByIndex(targetIndex);
+      if (obscured <= start높이) visibleSet.add(targetIndex);
+      obscured지도[targetIndex] = Math.max(높이, obscured);
 
-      이전비교 = 비교;
-      기존인덱스 = 대상인덱스;
+      previousComparison = comparison;
+      previousIndex = targetIndex;
     }
 
-    이전기준 = 지금기준;
+    previousReference = currentReference;
   }
 
-  return 보임집합;
+  return visibleSet;
 }
 
-const 직선방향들 = [
+const 직선Directions = [
   [ 1,1],[ 1,0],[ 1,-1],
   [-1,1],[-1,0],[-1,-1]
 ],
-기준직선방향들 = [
-  {기준방향: [ 0, 2], 직선방향들: [[-1, 1],[ 1, 1]]},
-  {기준방향: [ 2, 1], 직선방향들: [[ 1, 1],[ 1, 0]]},
-  {기준방향: [ 2,-1], 직선방향들: [[ 1, 0],[ 1,-1]]},
+reference직선Directions = [
+  {referenceDirection: [ 0, 2], 직선Directions: [[-1, 1],[ 1, 1]]},
+  {referenceDirection: [ 2, 1], 직선Directions: [[ 1, 1],[ 1, 0]]},
+  {referenceDirection: [ 2,-1], 직선Directions: [[ 1, 0],[ 1,-1]]},
 
-  {기준방향: [ 0,-2], 직선방향들: [[ 1,-1],[-1,-1]]},
-  {기준방향: [-2,-1], 직선방향들: [[-1,-1],[-1, 0]]},
-  {기준방향: [-2, 1], 직선방향들: [[-1, 0],[-1, 1]]},
+  {referenceDirection: [ 0,-2], 직선Directions: [[ 1,-1],[-1,-1]]},
+  {referenceDirection: [-2,-1], 직선Directions: [[-1,-1],[-1, 0]]},
+  {referenceDirection: [-2, 1], 직선Directions: [[-1, 0],[-1, 1]]},
 ];
