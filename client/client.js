367dea8ae28cbd0a9b820b61e3eb80346fcdc2b6
coder1423 <add000775@gmail.com>
2024-10-04 11:01:03 +0900

    메인, 클라이언트 파일  통합

--- client/client.js
+++ client/client.js
@@ -2,14 +2,15 @@
 import {Interaction} from './interaction.js';
 import * as Map from '../map-data.js';
 
+addEventListener('load',() => new Client(), {once : true});
+
 /**
- * 서버통신, 데이터 테이블
+ * 브라우져 전역 데이터, 서버통신, 데이터 테이블
  */
 export class Client {
-  /**
-   * @param {HTMLElement} root
-   */
-  constructor(root) {
+  constructor() {
+    const root = document.querySelector('main');
+    if (!root) return;
     new Interaction(root, Map.size, Map.data, Map.palette);
   }
 }
